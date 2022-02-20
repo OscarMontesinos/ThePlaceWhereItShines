0550af05882969427e9951f10d43e1d44cfa5524
OscarMontesinos <79588290+OscarMontesinos@users.noreply.github.com>
2022-02-20 22:08:43 +0100

    Ultima salvada

--- ThePlaceWhereItShines/Assets/Scripts/Empezar.cs
+++ ThePlaceWhereItShines/Assets/Scripts/Empezar.cs
@@ -5,8 +5,17 @@ using UnityEngine.SceneManagement;
 
 public class Empezar : MonoBehaviour
 {
-    public void Starting()
+    public GameObject panel;
+    public AudioSource audioCine;
+    public void Empieza()
     {
+        StartCoroutine(Starting());
+    }
+    public IEnumerator Starting()
+    {
+        panel.SetActive(true);
+        audioCine.Play();
+        yield return new WaitForSeconds(23);
         SceneManager.LoadScene("SampleScene", LoadSceneMode.Single);
     }
 }
